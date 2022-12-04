70e97c907a3324dcb42112362f6aa1f3e5be0dc7
Olga Gavrushenko <ogavby@gmail.com>
2022-12-03 21:16:23 -0800

    implemented view command-line options

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 // Packages needed for this application
 let inquirer = require('inquirer');
 const mysql = require('mysql2');
-const cTable = require('console.table');
-const optionsToChoose = [
-  "view all departments", 
-  "view all roles", 
-  "view all employees", 
-  "add a department", 
-  "add a role", 
-  "add an employee",
-  "update an employee role",
-  "update employee managers",
-  "view employees by manager",
-  "view employees by department",
-  "delete department",
-  "delete role",
-  "delete employee",
-  "view the total utilized budget of a department",
+let Query = require('./lib/query');
+
+let query = new Query;
+console.log(query.viewAllRoles());
+
+const options = [
+  {
+    name: "view all departments",
+    query: query.viewAllDepartments(),
+  },
+  {
+    name: "view all roles", 
+    query: query.viewAllRoles(),
+  },
+  {
+    name: "view all employees", 
+    query: query.viewAllEmployees(),
+  },
+  {
+    name: "view employees by manager",
+    query: query.viewEmployeesByManager(),
+  },
+  {
+    name: "view employees by department",
+    query: query.viewEmployeesByDepartment(),
+  },
+  {
+    name: "view the total utilized budget of a department",
+    query: query.viewBudgetOfDepartment(),
+  }, 
+  {
+    name: "add a department", 
+    query: "",
+  }, 
+  {
+    name: "add a role", 
+    query: "",
+  }, 
+  {
+    name: "add an employee",
+    query: "",
+  }, 
+  {
+    name: "update an employee role",
+    query: "",
+  }, 
+  {
+    name: "update employee managers",
+    query: "",
+  }, 
+  {
+    name: "delete department",
+    query: "",
+  }, 
+  {
+    name: "delete role",
+    query: "",
+  }, 
+  {
+    name: "delete employee",
+    query: "",
+  },
 ];
+
+const optionsNames = options.map(option => option.name);
+const optionsToChoose = [...optionsNames, "Exit"];
 
 // create a connection to database
 const connection = mysql.createConnection({
@@ -39,12 +88,14 @@ const header = `
            |++++++|=|=|=|=|=| []|
 ___________|++++++|  _______|   _________   _________  _________
       _______________   ______________      ______________
-__________________  ___________    __________________    ____________`
+__________________  ___________    __________________    ____________
+
+`
 
 
 console.log(header);
 
-const initApp = () => {
+const askRequest = () => {
   inquirer.prompt([
     {name: "start",
     type: "list",
@@ -53,24 +104,155 @@ const initApp = () => {
     }
   ])
   .then((res) => {
-    console.log(res.start)
+    executeRequest(res.start);
   })
 }
 
-initApp();
-// ex1
-// var values = [
-//   ['max', 20],
-//   ['joe', 30]
-// ];
-// console.table(['name', 'age'], values);
-// ex2
-// console.table([
-//   {
-//     name: 'foo',
-//     age: 10
-//   }, {
-//     name: 'bar',
-//     age: 20
-//   }
-// ]);
+const executeRequest = (queryRequest) => {
+  switch (queryRequest) {
+    // view all departments
+    case options[0].name: 
+      connection.query(options[0].query, (err, results) => {
+        if (err) {
+          console.log("error in query sqlViewAllDepartments");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+      break;
+    // view all roles  
+    case options[1].name: 
+      connection.query(options[1].query, (err, results) => {
+        if (err) {
+          console.log("error in query");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+      break;
+    // view all employees  
+    case optionsNames[2]: 
+      connection.query(options[2].query, (err, results) => {
+        if (err) {
+          console.log("error in query");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+      break;
+    // view employees by manager
+    case optionsNames[3]: 
+      inquirer.prompt({
+        type: "input",
+        name: "manager_id",
+        message: "Please enter the id of the manager: "
+      }).then((res) => {
+        let manager_id = res.manager_id;
+        connection.query(options[3].query, manager_id, (err, results) => {
+          if (err) {
+            console.log("error in query");
+            return;
+          }
+          console.table(results);
+          askRequest();
+        })
+      })
+      break;
+    // view employee by department
+    case optionsNames[4]: 
+    inquirer.prompt({
+      type: "input", 
+      name: "department_id",
+      message: "Please enter the id of the department: "
+    }).then((res) => {
+      let department_id = res.department_id;
+      connection.query(options[4].query, department_id, (err, results) => {
+        if (err) {
+          console.log("error in query");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+    })
+      break;
+    case optionsNames[5]: 
+      connection.query(options[5].query, (err, results) => {
+        if (err) {
+          console.log("error in query");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+      break;
+    case optionsNames[6]: 
+      connection.query(options[6].query, (err, results) => {
+        if (err) {
+          console.log("error in query");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+      break;      
+    case optionsNames[7]: 
+      connection.query(options[7].query, (err, results) => {
+        if (err) {
+          console.log("error in query");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+      break;      
+    case optionsNames[8]: 
+      connection.query(options[8].query, (err, results) => {
+        if (err) {
+          console.log("error in query");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+      break;
+    case optionsNames[9]: 
+      connection.query(options[9].query, (err, results) => {
+        if (err) {
+          console.log("error in query");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+      break;
+    case optionsNames[10]: 
+      connection.query(options[10].query, (err, results) => {
+        if (err) {
+          console.log("error in query");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+      break;
+    case optionsNames[11]: 
+      connection.query(options[11].query, (err, results) => {
+        if (err) {
+          console.log("error in query");
+          return;
+        }
+        console.table(results);
+        askRequest();
+      })
+      break;
+    default:
+      console.log("no option selected");
+      console.log(queryRequest);
+  }
+}
+
+askRequest();
