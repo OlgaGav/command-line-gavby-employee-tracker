e00910b1e20433aa9775ef55f8a18a104382ac3d
Olga Gavrushenko <ogavby@gmail.com>
2022-12-07 12:50:00 -0800

    updated readme and apply code formatting

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const inquirer = require('inquirer');
-const Query = require('./lib/query');
-const View = require('./lib/view');
-const ViewEmployeesByManager = require('./lib/viewEmployeesByManager');
-const ViewEmployeesByDepartment = require('./lib/viewEmployeesByDepartment');
-const UpdateEmployeeManager = require('./lib/UpdateEmployeeManager');
-const UpdateEmployeeRole = require('./lib/UpdateEmployeeRole');
-const AddRole = require('./lib/addRole');
-const AddEmployee = require('./lib/addEmployee');
-const AddDepartment = require('./lib/addDepartment');
-const DeleteRole = require('./lib/deleteRole');
-const DeleteEmployee = require('./lib/deleteEmployee');
-const DeleteDepartment = require('./lib/deleteDepartment');
+const inquirer = require("inquirer");
+const Query = require("./lib/query");
+const View = require("./lib/view");
+const ViewEmployeesByManager = require("./lib/viewEmployeesByManager");
+const ViewEmployeesByDepartment = require("./lib/viewEmployeesByDepartment");
+const UpdateEmployeeManager = require("./lib/UpdateEmployeeManager");
+const UpdateEmployeeRole = require("./lib/UpdateEmployeeRole");
+const AddRole = require("./lib/addRole");
+const AddEmployee = require("./lib/addEmployee");
+const AddDepartment = require("./lib/addDepartment");
+const DeleteRole = require("./lib/deleteRole");
+const DeleteEmployee = require("./lib/deleteEmployee");
+const DeleteDepartment = require("./lib/deleteDepartment");
 const query = new Query();
 const header = `
                              ^
@@ -30,25 +30,25 @@ __________________  ___________    __________________    ____________
 `;
 
 let userOptions = [];
-let viewAllDepartments = new View(`01`, `view all departments`, query.viewAllDepartments());
-let viewAllRoles = new View(`02`, `view all roles`, query.viewAllRoles());
-let viewAllEmployees = new View(`03`, `view all employees`, query.viewAllEmployees());
-let viewEmployeesByManager = new ViewEmployeesByManager(`04`, `view employees by manager`, query.viewEmployeesByManager());
-let viewEmployeesByDepartment = new ViewEmployeesByDepartment(`05`, `view employees by department`, query.viewEmployeesByDepartment());
-let viewBudgetOfDepartment = new View(`06`, `view the total utilized budget of a department`, query.viewBudgetOfDepartment());
-let updateEmployeeManager = new UpdateEmployeeManager(`07`, `update the manager of employee`, query.updateEmployeeManager());
-let updateEmployeeRole = new UpdateEmployeeRole(`08`, `update the role of employee`, query.updateEmployeeRole());
-let addDepartment = new AddDepartment(`09`, `add new department`, query.addDepartment());
-let addRole = new AddRole(`10`, `add new role`, query.addRole());
-let addEmployee = new AddEmployee(`11`, `add new employee`, query.addEmployee());
-let deleteDepartment = new DeleteDepartment(`12`, `delete department`, query.deleteDepartment());
-let deleteRole = new DeleteRole(`13`, `delete role`, query.deleteRole());
-let deleteEmployee = new DeleteEmployee(`14`, `delete employee`, query.deleteEmployee());
+let viewAllDepartments = new View(`01`,`View all departments`, query.viewAllDepartments());
+let viewAllRoles = new View(`02`, `View all roles`, query.viewAllRoles());
+let viewAllEmployees = new View(`03`, `View all employees`, query.viewAllEmployees());
+let viewEmployeesByManager = new ViewEmployeesByManager(`04`, `View employees by manager`, query.viewEmployeesByManager());
+let viewEmployeesByDepartment = new ViewEmployeesByDepartment(`05`, `View employees by department`, query.viewEmployeesByDepartment());
+let viewBudgetOfDepartment = new View(`06`, `View the total utilized budget of a department`, query.viewBudgetOfDepartment());
+let updateEmployeeManager = new UpdateEmployeeManager(`07`, `Update the manager of employee`, query.updateEmployeeManager());
+let updateEmployeeRole = new UpdateEmployeeRole(`08`, `Update the role of employee`, query.updateEmployeeRole());
+let addDepartment = new AddDepartment(`09`, `Add new department`, query.addDepartment());
+let addRole = new AddRole(`10`, `Add new role`, query.addRole());
+let addEmployee = new AddEmployee(`11`, `Add new employee`, query.addEmployee());
+let deleteDepartment = new DeleteDepartment(`12`, `Delete department`, query.deleteDepartment());
+let deleteRole = new DeleteRole(`13`, `Delete role`, query.deleteRole());
+let deleteEmployee = new DeleteEmployee(`14`, `Delete employee`, query.deleteEmployee());
 
 userOptions.push(
-  viewAllDepartments, 
-  viewAllRoles, 
-  viewAllEmployees, 
+  viewAllDepartments,
+  viewAllRoles,
+  viewAllEmployees,
   viewEmployeesByManager,
   viewEmployeesByDepartment,
   viewBudgetOfDepartment,
@@ -59,11 +59,11 @@ userOptions.push(
   addEmployee,
   deleteDepartment,
   deleteRole,
-  deleteEmployee,
-  );
+  deleteEmployee
+);
 
-const options = userOptions.map(option => option.name);
-const listOfOptions = [...options, "Close"]
+const options = userOptions.map((option) => option.name);
+const listOfOptions = [...options, "Quit"];
 
 const askUser = async () => {
   let res = await inquirer.prompt([
@@ -72,33 +72,37 @@ const askUser = async () => {
       name: "option",
       message: "What woud you like to do?",
       choices: listOfOptions,
-    }
-  ])
-  if (res.option === "Close") {
-    wantToExit();
+    },
+  ]);
+  if (res.option === "Quit") {
+    wantToQuit();
   } else {
-    const selectedOption = userOptions.find(option => option.name === res.option);
+    const selectedOption = userOptions.find(
+      (option) => option.name === res.option
+    );
     showResult(selectedOption);
   }
-}
+};
 
 const showResult = async (selectedOption) => {
   await selectedOption.executeQuery();
   await askUser();
-}
+};
 
-const wantToExit = () =>
+const wantToQuit = () =>
   inquirer
     .prompt([
       {
-        name: "exit",
+        name: "quit",
         type: "confirm",
-        message: "Do you want to exit the application?",
+        message: "Do you want to quit the application?",
       },
     ])
     .then((answer) => {
-      if (answer.exit) {
-        console.log("Thank you for using this application. Press CRTL+C to end the process")
+      if (answer.quit) {
+        console.log(
+          "Thank you for using this application. Press CRTL+C to end the process"
+        );
       } else {
         askUser();
       }
